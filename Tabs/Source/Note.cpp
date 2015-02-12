841adbec5acd96ec56b0ff15bce9095d1d83a05a
Neil Stoker <neil.stoker@gmail.com>
2015-02-12 09:23:02 +0000

    Initial Note class code completed

--- Tabs/Source/Note.cpp
+++ Tabs/Source/Note.cpp
@@ -9,3 +9,24 @@
 */
 
 #include "Note.h"
+
+Note::Note(const String& name, const String& contents)
+	: textValueObject(contents)
+{
+	setName(name);
+	editor.setMultiLine(true);
+	editor.setReturnKeyStartsNewLine(true);
+	editor.getTextValue().referTo(textValueObject);
+	addAndMakeVisible(editor);
+	editor.addListener(this);
+}
+
+Note::~Note()
+{
+	editor.removeListener(this);
+}
+
+void Note::textEditorTextChanged(TextEditor&) {}
+void Note::textEditorReturnKeyPressed(TextEditor&) {}
+void Note::textEditorEscapeKeyPressed(TextEditor&) {}
+void Note::textEditorFocusLost(TextEditor&) {}
--- Tabs/Source/Note.h
+++ Tabs/Source/Note.h
@@ -11,7 +11,27 @@
 #ifndef NOTE_H_INCLUDED
 #define NOTE_H_INCLUDED
 
+#include "../JuceLibraryCode/JuceHeader.h"
 
+class Note
+	: public Component
+	, private TextEditor::Listener
+{
+public:
+	Note(const String& name, const String& contents);
+	~Note();
+
+private:
+	Value textValueObject;
+	TextEditor editor;
+
+	void textEditorTextChanged(TextEditor&) override;
+	void textEditorReturnKeyPressed(TextEditor&) override;
+	void textEditorEscapeKeyPressed(TextEditor&) override;
+	void textEditorFocusLost(TextEditor&) override;
+
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Note);
+};
 
 
 
