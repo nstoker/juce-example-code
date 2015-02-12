3f2eff8ab7079bf819a98da3a2f16ec8852510ab
Neil Stoker <neil.stoker@gmail.com>
2015-02-12 08:45:34 +0000

    Initial attempt at redoing tabs control

---
+++ Tabs/Source/MainComponent.cpp
@@ -0,0 +1,46 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#include "MainComponent.h"
+
+
+//==============================================================================
+MainContentComponent::MainContentComponent()
+{
+	showInTabsButton.setButtonText("Show with tabs");
+	showInTabsButton.setToggleState(false, dontSendNotification);
+	showInTabsButton.addListener(this);
+	addAndMakeVisible(showInTabsButton);
+
+	addTabButton.setButtonText("Add Tab");
+	addTabButton.addListener(this);
+	addAndMakeVisible(addTabButton);
+
+    setSize (600, 400);
+}
+
+MainContentComponent::~MainContentComponent()
+{
+}
+
+void MainContentComponent::paint (Graphics& g)
+{
+    g.fillAll (Colours::azure);
+
+    g.setFont (Font (16.0f));
+    g.setColour (Colours::white);
+    g.drawText ("Hello World!", getLocalBounds(), Justification::centred, true);
+}
+
+void MainContentComponent::resized()
+{
+	Rectangle<int>area(getLocalBounds());
+	Rectangle<int> buttonArea(area.removeFromTop(28).reduced(2));
+	addTabButton.setBounds(buttonArea.removeFromRight(150));
+	showInTabsButton.setBounds(buttonArea);
+}
