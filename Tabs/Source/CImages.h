159f5e7adce768a0e7347759f775cd1ca25236fc
Neil Stoker <neil.stoker@gmail.com>
2015-02-13 11:33:58 +0000

    Add image class

---
+++ Tabs/Source/CImages.h
@@ -0,0 +1,16 @@
+/* (Auto-generated binary data file). */
+
+#ifndef BINARY_CIMAGES.H_H
+#define BINARY_CIMAGES.H_H
+
+namespace CImages.h
+{
+    extern const char*  palintest_pool_and_spa_logo_on_background_cmyk_png;
+    const int           palintest_pool_and_spa_logo_on_background_cmyk_pngSize = 8690;
+
+    extern const char*  twiterbg_png;
+    const int           twiterbg_pngSize = 119928;
+
+}
+
+#endif
